b85ee09d0d40c5f5265c01eaecd26c7b43fbc5f7
Dmitriy Kuzmenko <dmitriy.kuzmenko.ss@gmail.com>
2022-08-19 14:17:45 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import { ChangeEvent } from 'react';
+
 const App = () => {
-  return (
-    <div className="App">
-      Test
-    </div>
-  );
-}
+	const convertFileToBase64 = (file: File) => {
+		return new Promise((resolve, reject) => {
+			if (!file) return;
+			const fileReader = new FileReader();
+			fileReader.readAsDataURL(file);
+
+			fileReader.onload = () => {
+				resolve(fileReader.result);
+			};
+			fileReader.onerror = (error) => {
+				reject(error);
+			};
+		});
+	};
+
+	const onMediaChange = async (e: ChangeEvent<HTMLInputElement>) => {
+		if (!e.target.files) return;
+		const file = e.target.files[0];
+		const base64 = await convertFileToBase64(file);
+		alert(base64);
+	};
+
+	return (
+		<div className='App'>
+			Test
+			<input
+				type='file'
+				accept='image/x-png,image/jpeg,image/gif'
+				onChange={onMediaChange}
+			/>
+		</div>
+	);
+};
 
 export default App;
