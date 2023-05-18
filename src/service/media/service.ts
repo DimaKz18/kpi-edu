614671f5ef9025502836399304c6f517752beac3
Dmitriy Kuzmenko <dmitriy.kuzmenko.ss@gmail.com>
2023-05-18 13:58:04 +0300

    feat(explore page): added query request

---
+++ src/service/media/service.ts
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { protectedQuery } from 'service/api';
+import { Media } from './models';
+
+// export const mediaService = createApi({
+// 	reducerPath: 'mediaService',
+// 	baseQuery: protectedQuery,
+// 	endpoints: (build) => ({`
+// 		fetchMedia: build.query<Media[], string>({
+// 			query: (search: string) => ({
+// 				url: '/profile',
+// 			}),
+// 		}),
+// 	}),
+// });
+
+export const mediaService = createApi({
+	reducerPath: 'mediaService',
+	refetchOnFocus: true,
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'https://api.github.com',
+	}),
+	endpoints: (build) => ({
+		fetchMedias: build.query<Media[], string>({
+			query: (search: string) => ({
+				url: '/search/users',
+				params: {
+					q: search,
+					per_page: 10,
+				},
+			}),
+			transformResponse: (response: any) => response.items,
+		}),
+	}),
+});
+
+export const { useFetchMediasQuery, useLazyFetchMediasQuery } = mediaService;
+
